445701503beb325dc5352542e33b593bf93f3e9b
Timothy Wroge <tiw51@pitt.edu>
2021-07-10 18:29:58 -0400

    fixes: add better propagaation of error for scalar multiplication

--- src/uncertainty/include/uncertainty/uncertainty.hh
+++ src/uncertainty/include/uncertainty/uncertainty.hh
@@ -1,5 +1,6 @@
 #pragma once
 #include <cmath>
+#include <iostream>
 
 namespace uncertainty {
 
@@ -12,30 +13,45 @@ template <typename Scalar>
 struct Measurement
 {
 
+    private:
+        Scalar mu;
+        Scalar variance;
+
     public:
         Measurement(Scalar mu, Scalar variance) : mu(mu), variance(variance) {};
 
         Measurement operator + (const Measurement & B) const {
-            return {mu + B.mu, variance + other.variance};
+            return {mu + B.mu, variance + B.variance};
         }
 
         Measurement operator * (const Scalar & a) const {
-            return {mu, Square(a) * variance};
+            return {a * mu, Square(a) * variance};
         }
 
         Measurement operator * (const Measurement & B) const {
-            constexpr auto f = mu * B.mu
+            auto f = mu * B.mu;
             return {f, Square(f) * (Square(variance / mu) + Square(B.variance / B.mu)) };
         }
 
         Measurement operator ^ (const Scalar b) const {
-            constexpr auto f = pow(mu, b);
+            auto f = pow(mu, b);
             return {f, Square ( b * pow(mu, b - 1) * variance ) };
         }
 
-    private:
-            Scalar mu;
-            Scalar variance;
-}
+
+        friend std::ostream& operator << (std::ostream & os, const Measurement m){
+            os << m.mu << " ± " << m.variance ;
+            return os;
+        }
+
+        Scalar Mu(){
+            return mu;
+        };
+
+        Scalar Variance(){
+            return variance;
+        };
+
+};
 
 }// namespace uncertainty
---
+++ test/test_uncertainty.cc
@@ -0,0 +1,14 @@
+#include <uncertainty/uncertainty.hh>
+using namespace uncertainty;
+
+int main(void) {
+    Measurement A = {3, 4};
+    Measurement B = {1, 4};
+    auto C = A + B;
+    auto D = A * B;
+
+    std::cout << "A: "<< A << std::endl;
+    std::cout << "C: "<< C << std::endl;
+    std::cout << "D: "<< D << std::endl;
+    std::cout << "A * 2: "<< A*2  << std::endl;
+}

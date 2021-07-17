c55917942f6af325c52d5ce4d2e226b5874ac6ae
Timothy Wroge <tiw51@pitt.edu>
2021-07-17 11:13:59 -0400

    feat: add multivariate measurements

--- src/uncertainty/include/uncertainty/uncertainty.hh
+++ src/uncertainty/include/uncertainty/uncertainty.hh
@@ -24,6 +24,10 @@ struct Measurement
             return {mu + B.mu, variance + B.variance};
         }
 
+        Measurement operator + (const Scalar & b) const {
+            return {mu + b, variance};
+        }
+
         Measurement operator * (const Scalar & a) const {
             return {a * mu, Square(a) * variance};
         }
@@ -33,7 +37,7 @@ struct Measurement
             return {f, Square(f) * (Square(variance / mu) + Square(B.variance / B.mu)) };
         }
 
-        friend Measurement operator * (const Scalar & a, const Measurement & A) const {
+        friend Measurement operator * (const Scalar & a, const Measurement & A) {
             return A * a;
         }
 
@@ -54,6 +58,36 @@ struct Measurement
 
         Scalar Variance(){
             return variance;
+        };
+
+};
+
+template <typename Array>
+struct Measurements
+{
+    private:
+        Array mu;
+        Array covariance;
+
+    public:
+        Measurements(Array mu, Array covariance) : mu(mu) , covariance(covariance) {};
+
+        friend Measurements operator * (const Array & transformation, const Measurements & measurements){
+            // this is a linear transofrmation
+            return {transformation * measurements.mu, transformation * measurements.covariance * transformation.inverse() };
+        };
+
+        Measurements operator + (const Measurements & B){
+            // this is a linear transofrmation
+            return {mu + B.mu, covariance  + B.covariance};
+        };
+
+        Array Mu(){
+            return mu;
+        };
+
+        Array Covariance(){
+            return covariance;
         };
 
 };
